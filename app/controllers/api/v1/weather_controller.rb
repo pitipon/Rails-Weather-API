f205da98de03f4a6d3b890f86145719881f295bb
Pitipon Pluemworasawat <pitipon@gmail.com>
2018-08-09 14:15:18 +0800

    env

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,6 +1,18 @@
 # app/controllers/api/v1/weather_controller.rb 
+require 'rest-client'
+require 'json'
+
 class Api::V1::WeatherController < Api::V1::BaseController 
     def search 
-        p "hello world"
+        puts params
+        response = RestClient.get("https://api.darksky.net/forecast/#{ENV['key']}/42.3601,-71.0589")
+        # puts "Code: #{response.code}"
+        
+        # Change String to JSON
+        result = JSON.parse(response.body)
+        p result["daily"]["summary"]
+        render json: {
+            "weather": result
+        }
     end 
-  end
+end
